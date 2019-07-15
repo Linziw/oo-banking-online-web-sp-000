6f3f3cf99aedf9edd4ee441fa2858595ab609dcb
Lindsey Wells <lindsey.wells84@gmail.com>
2019-07-15 14:16:39 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -3,10 +3,7 @@ class Transfer
   
   attr_accessor :sender, :receiver, :amount
   
-  def inititalize(sender, receiver, amount)
-    @sender=sender
-    @receiver = receiver
-    @amount = amount
-    binding.pry
+  def inititalize
   end
+
 end
